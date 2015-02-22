ed503cb7601220e72d01b4f8567cea6027592c3d
Alex Libman <kingalex11235@yahoo.com>
2015-02-21 20:54:50 -0500

    finish socket io instructions

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
-var io = require('socket.io');
-
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 //required to render static files
 app.use(express.static(__dirname + "/static"));
@@ -11,5 +11,18 @@ app.get('/',function(req,res){
     res.render("index.html");
 });
 
+io.on('connection', function(socket){
+    console.log('A user connected');
+    socket.on('msg', function(msg){
+        io.emit("msg", msg);
+        console.log('message: ' + msg);
+    });
+    socket.on('disconnect', function(){
+        console.log('User disconnected');
+    });
+});
+
 //run on 127.0.0.1:5000
-app.listen(process.env.PORT || 5000);
+http.listen(5000, function(){
+    console.log("Server started on port 5000");
+});
